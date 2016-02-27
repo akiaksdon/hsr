97c6b2f66f104d0ecfaac2763b4400abb4d1723d
xcavator2011 <xcavator2011@gmail.com>
2016-02-27 20:48:38 +0530

    Login.js commented
    Comments added for ease of debugging and understanding.

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,21 +1,21 @@
 import Ember from 'ember';
-var LoginController = Ember.Controller.extend({
-  session: Ember.inject.service(),
-  errorMessage: null,
-  identification: null,
-  password: null,
+var LoginController = Ember.Controller.extend({/*Extended Ember Controller into LoginController*/
+  session: Ember.inject.service(),/*Session created for the page to display using ember*/
+  errorMessage: null,/*INIT*/
+  identification: null,/*INIT*/
+  password: null,/*INIT*/
 
-  actions: {
-    authenticate() {
-      let { identification, password } = this.getProperties('identification', 'password');
-      this.get('session').authenticate('authenticator:custom', {
-        identification: identification,
-        password: password
-      }).catch((error) => {
-        this.set('errorMessage', error.reason);
-      });
-    }
-  }
-});
+  actions: {/* Actions to be defined after performing clicks on the page*/
+    authenticate() {/*Authenticate function begins*/
+      let { identification, password } = this.getProperties('identification', 'password');/* caught parameters from the page and stored locally */
+      this.get('session').authenticate('authenticator:custom', {//Authenticate the session using custom authentication provided in hsr/app/authenticators/
+        identification: identification,//locally storing values returned from the page
+        password: password//locally storing values returned from the page
+      }).catch((error) => {//try catch block for safe exit and error display **USEFUL FOR DEBUGGIN**
+        this.set('errorMessage', error.reason);//catching the error in error.reason variable
+      });//EOcatch
+    }//EOauthenticate
+  }//EOActions Block
+});//EOLoginController
 
-export default LoginController;
+export default LoginController;//Keep LoginController to be resuable within the code
